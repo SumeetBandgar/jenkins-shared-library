b3b39ffefb8efcf4392dfca49dd3abe72a77fb52
Sumeet Bandgar <sumeetbandgar@gmail.com>
2022-11-14 09:21:17 +0530

    updated parameters with quotes

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,6 +2,6 @@ def build() {
   sh 'mvn clean install'
 }
 
-def checkout(String url, String branch) {
-    git branch: ${branch}, url: ${url}
+def checkout(url, branch) {
+    git branch: '${branch}', url: '${url}'
 }
